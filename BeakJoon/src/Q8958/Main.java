4b9a3d5a87e57f4bb1cb9069b999fac0ad465d6a
21-manic-014g <21-manic-014g@21-manic-014>
2023-10-02 17:40:50 +0900

    2023.10.02 추가

--- BeakJoon/src/Q8958/Main.java
+++ BeakJoon/src/Q8958/Main.java
@@ -5,17 +5,27 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		/*
+		
 		int T = sc.nextInt();
 		System.out.println(T);
 
 		for (int t = 0; t < T; t++) {
 			String score = sc.next();
-			System.out.println(score);
+			
+			int sum = 0, cnt = 1;
+			for(int i=0; i<score.length(); i++) {
+		
+				if(score.charAt(i) == 'O') {
+					sum += cnt++;
+				}else {
+					cnt = 1;
+				}
+			}
+			System.out.println(sum);
 		}
-		*/
+		sc.close();
 
+		/*
 		String str[] = new String[sc.nextInt()];
 
 		for (int i = 0; i < str.length; i++) {
@@ -38,5 +48,6 @@ public class Main {
 			}
 			System.out.println(sum);
 		}
+		*/
 	}
 }
