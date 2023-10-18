7aa2a16785d8dbb662776ad12d9983ffb4d52e39
allis <allis@LAPTOP-H71R1AEC>
2023-10-18 21:05:35 +0900

    2023.10.18

---
+++ BeakJoon/src/Q1000/Main.java
@@ -0,0 +1,14 @@
+package Q1000;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a+b);
+	}
+}
